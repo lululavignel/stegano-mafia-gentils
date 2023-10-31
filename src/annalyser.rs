22badb0aaabd116f8b4ccbd6496dda0f95f53486
Thomas Sagot <79260915+Thomas-je@users.noreply.github.com>
2023-10-31 10:25:35 +0100

    added annalysers

---
+++ src/annalyser.rs
@@ -0,0 +1,116 @@
+
+
+use image::{RgbImage, ImageBuffer, Rgb};
+
+pub fn stats_last_bits(img : & RgbImage,bytes: i32)->Vec<u32>{
+    let array_size= i32::pow(2,bytes as u32) as usize;
+    let mut bits_count= vec![0;array_size];
+    let mut mask :u8 =0;
+    for _ in 0..bytes{
+        mask=(mask<<1)|1;
+    } 
+    for (_,_,pixel) in img.enumerate_pixels(){
+        for val in pixel.0{
+            bits_count[(val&mask)as usize]+=1
+        }
+    }
+    return bits_count;
+
+}
+pub fn diff_img_create(img : & RgbImage, bytes : i32)-> RgbImage{
+    if (bytes<0) & (bytes>=8){
+        panic!("bytes must be positive and strictly inferior to 9");
+    }
+    let mut msk: u8 = 0x1;
+    for _ in 1..bytes{
+        msk|=msk<<1;
+    }
+    let shift = (8-bytes) as u8;
+    let (x,y)=img.dimensions();
+    let mut new_img: RgbImage = ImageBuffer::new(x-1, y-1);
+    for i in 1..x-1{
+        for j in 1..y-1{
+            
+            let img_cur_pixel=img.get_pixel(i, j);
+            let mut diffs :[[Rgb<u8>;2];2]=[[Rgb::<u8>{0:[0,0,0]};2];2];
+            for di in 0..2{
+                for dj in 0..2 {
+                    if di!=0 &&dj!=0{
+                        diffs[di][dj]=distance(
+                            img_cur_pixel,img.get_pixel(i-(di as u32),j-(dj as u32)),msk,shift );
+                    }
+                }
+            }
+            new_img.put_pixel(i, j, diffs[1][1]);
+
+            //let curs_pixel= ((-1..1),(1..1)).map(dx,dy).m;
+
+        }
+    }
+    return new_img;
+}
+
+fn distance(p1: &Rgb<u8>, p2: &Rgb<u8>,msk:u8,shift:u8)-> Rgb<u8>{
+    let mut difference;
+    let mut rgb=Rgb::<u8>{0:[0;3]};
+    for i in 0..3{
+        let cur_color_diff=(p1.0[i] as i32)-(p2.0[i] as i32);
+        difference= cur_color_diff*cur_color_diff;
+        if difference > 0xFF{
+            difference=0xFF;
+        }
+        difference&=msk as i32;
+        rgb.0[i]=(difference as u8)<<shift;
+        
+    }
+    return rgb;
+}
+
+#[cfg(test)]
+mod test_annalyzer{
+    
+
+    use super::*;
+    use image::io::Reader as ImageReader;
+    #[test]
+    fn test_key_io(){
+        let image1 = ImageReader::open("./photo.jpg").unwrap().decode().unwrap().to_rgb8();
+        let image2 = ImageReader::open("./steg_with_key.jpg").unwrap().decode().unwrap().to_rgb8();
+        let image3 = ImageReader::open("./steg.jpg").unwrap().decode().unwrap().to_rgb8();
+        
+        let res1 = stats_last_bits(&image1,2);
+        let res2 = stats_last_bits(&image2,2);
+        let res3 = stats_last_bits(&image3,2);
+        println!("original:              {:?}", res1);
+        println!("modified:              {:?}", res2);
+        println!("modified without key:  {:?}", res3);
+    }
+
+    #[test]
+    fn try_img_delta(){
+        /*
+        let image= ImageReader::open("./img/photo/celeste-.png").unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/celeste-_2.png").unwrap();
+        let cur_str="origami.png";
+        let image= ImageReader::open("./img/photo/".to_owned()+cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/".to_owned()+ cur_str).unwrap();
+        let cur_str="celeste-3.png";
+        let image= ImageReader::open("./img/photo/".to_owned()+cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/".to_owned()+ cur_str).unwrap();
+        let cur_str="steg_with_key.png";
+        let image= ImageReader::open(cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/".to_owned()+ cur_str).unwrap();
+        let cur_str="cc.png";
+        let image= ImageReader::open(cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/".to_owned()+ cur_str).unwrap();
+        */
+        let cur_str="vile-foret-hash512.png";
+        let image= ImageReader::open("./img/photo/secret/".to_owned()+cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/".to_owned()+ cur_str).unwrap();
+        let cur_str="maddy-pfff.png";
+        let image= ImageReader::open("./img/photo/secret/".to_owned()+cur_str).unwrap().decode().unwrap().to_rgb8();
+        diff_img_create(&image, 2).save("./img/photo/trnsfrm/sec-".to_owned()+ cur_str).unwrap();
+        
+    }
+
+}
