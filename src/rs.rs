94843becb896f74bf736e20374335e59edae6e9c
Thomas-je <thomas.sagotabc@gmail.com>
2024-03-29 17:06:18 +0100

    op steganlysis method (RS)

--- src/rs.rs
+++ src/rs.rs
@@ -1,4 +1,6 @@
 use image::{GenericImageView, RgbImage};
+
+use crate::shannon_entropy::randomize_lsb;
 pub enum Group{
     Regular,
     Singular,
@@ -68,13 +70,120 @@ pub fn group_stats(img:&RgbImage,pos_mask:&[&[i16]])-> (f64,f64,f64,i32){
         }
     }
     let total_f = total as f64;
-    return (singular_c as f64/total_f,regular_c as f64/total_f,unusable_c as f64/total_f,total);
+    return (regular_c as f64/total_f,singular_c as f64/total_f,unusable_c as f64/total_f,total);
 
 }
 
-pub fn compute_rs(img:&RgbImage,mask:&[&[i8]]) ->f64{
+pub fn compute_rs(img:&RgbImage,mask:&[&[i16]],verbose:bool) ->Option::<f64>{
+
+    let positive_mask= mask;
+    let mut negative_mask= Vec::<Vec::<i16>>::new();
+    let mut line_v;
+    for line in 0..positive_mask.len(){
+        line_v = Vec::<i16>::with_capacity(positive_mask[line].len());
+        for column in 0..positive_mask[line].len(){
+            line_v.push(positive_mask[line][column]  * -1);
+        }
+        negative_mask.push(line_v);
+    }
+    let mut neg = Vec::<&[i16]>::new();
+    for a in &negative_mask{
+        neg.push(a);
+    }
+    let stats_p2 = group_stats(img, mask);
+    let neg_stats_p2 = group_stats(img, neg.as_slice());
+
+    let mut img_1_minus_p2= img.clone();
+    randomize_lsb(&mut img_1_minus_p2,1.0,0x01 );
+    let stats_1_minus_p2 = group_stats(img, mask);
+    let neg_stats_1_minus_p2 = group_stats(img, neg.as_slice());
+
+    let r_m_p_2=  stats_p2.0;
+    let s_m_p_2=  stats_p2.1;
+
+    let r_m_1_p_2=  stats_1_minus_p2.0;
+    let s_m_1_p_2=  stats_1_minus_p2.1;
+
+    let r_neg_m_p_2=  neg_stats_p2.0;
+    let s_neg_m_p_2=  neg_stats_p2.1;
+
+    let r_neg_m_1_p_2=  neg_stats_1_minus_p2.0;
+    let s_neg_m_1_p_2=  neg_stats_1_minus_p2.1;
 
 
-    let steganographied_percentage=0.;
-    return steganographied_percentage;
+    let d_0 = r_m_p_2 -s_m_p_2;
+    let d_1 = r_m_1_p_2 -s_m_1_p_2;
+
+    let d_neg_0 = r_neg_m_p_2 -s_neg_m_p_2;
+    let d_neg_1 = r_neg_m_1_p_2 -s_neg_m_1_p_2;
+
+    let a = 2.*(d_1+d_0);
+    let b= d_neg_0 - d_neg_1 - d_1 - 3.*d_0;
+    let c = d_0- d_neg_0; 
+
+    let delta = b*b  - 4.*a*c ;
+    if verbose{
+        println!("Polynome : {}X² + {}X + {}",a,b,c);
+    }
+
+    if delta<0.{
+        return None;
+    }
+    let x;
+    if delta==0.{
+        x=-b/(2.*a);
+        return Some(x/(x-(1./2.)));
+    }
+
+    let x1=(-b+delta.sqrt())/(2.*a);
+    let x2=(-b-delta.sqrt())/(2.*a);
+   
+    let z1=x1/(x1-(1./2.));
+    let z2=x2/(x2-(1./2.));
+    if verbose{
+        println!("X1::: {x1}");
+        println!("X2::: {x2}");
+        println!("Z1:: {}",z1);
+        println!("Z2:: {}",z2);
+    }
+    if z1<0.{
+        return Some(z2);
+    }
+    if z2>0. && z2<z1{
+        return Some(z2);
+    }
+    return Some(z1);
+
+
+    
+}
+
+
+
+
+
+#[cfg(test)]
+mod test_entropy{
+
+   
+    use image::io::Reader as ImageReader;
+    use super::*;
+
+
+    #[test]
+    fn test_compute_rs(){
+        let values = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99];
+        for value in values{
+            let img_name=format!("/home/admin/Images/steg/s256-c-p-{}/IMG_20231005_133114.png",value);
+            let image = ImageReader::open(img_name).unwrap().decode().unwrap().to_rgb8();
+
+            let mask= [[0 as i16 ,1,1,0].as_slice()];
+            let res = compute_rs(&image,mask.as_slice(),false);
+
+            println!("========================\nResult for {}% steganographied : {}",value,res.unwrap());
+        }
+        
+        
+
+    } 
 }
